4006abd64057f4f00637d56c50a4e88fcf675cf3
Gibirizon <swojteksz@gmail.com>
2024-09-11 16:00:45 +0200

    fixing weird lsp bugs

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,16 @@
 return {
     {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v4.x",
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v4.x',
         lazy = true,
         config = false,
     },
     {
-        "williamboman/mason.nvim",
+        'williamboman/mason.nvim',
         lazy = false,
         config = true,
     },
+
     {
         "jay-babu/mason-null-ls.nvim",
         event = { "BufReadPre", "BufNewFile" },
@@ -60,52 +61,52 @@ return {
 
     -- Autocompletion
     {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
         dependencies = {
-            { "L3MON4D3/LuaSnip" },
-            { "hrsh7th/cmp-nvim-lsp-signature-help" }, 
+            {'L3MON4D3/LuaSnip'},
+            { "hrsh7th/cmp-nvim-lsp-signature-help" },
         },
         config = function()
-            local cmp = require "cmp"
+            local cmp = require('cmp')
 
-            cmp.setup {
+            cmp.setup({
                 sources = {
-                    { name = "nvim_lsp" },
+                    {name = 'nvim_lsp'},
                     { name = 'nvim_lsp_signature_help' },
                 },
-                mapping = cmp.mapping.preset.insert {
-                    ["<C-Space>"] = cmp.mapping.complete(),
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
                     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
                     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
                     ["<Enter>"] = cmp.mapping.confirm { select = true },
-                },
+                }),
                 snippet = {
                     expand = function(args)
                         vim.snippet.expand(args.body)
                     end,
                 },
-            }
-        end,
+            })
+        end
     },
 
     -- LSP
     {
-        "neovim/nvim-lspconfig",
-        cmd = "LspInfo",
-        event = { "BufReadPre", "BufNewFile" },
+        'neovim/nvim-lspconfig',
+        cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+        event = {'BufReadPre', 'BufNewFile'},
         dependencies = {
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
         },
         config = function()
-            local lsp_zero = require "lsp-zero"
+            local lsp_zero = require('lsp-zero')
 
             -- lsp_attach is where you enable features that only work
             -- if there is a language server active in the file
             local lsp_attach = function(client, bufnr)
-                local opts = { buffer = bufnr }
+                local opts = {buffer = bufnr}
 
                 vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
                 vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
@@ -116,24 +117,24 @@ return {
                 vim.keymap.set({ "n", "x" }, "<leader>f", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
             end
 
-            lsp_zero.extend_lspconfig {
+            lsp_zero.extend_lspconfig({
                 sign_text = true,
                 lsp_attach = lsp_attach,
-                capabilities = require("cmp_nvim_lsp").default_capabilities(),
-            }
+                capabilities = require('cmp_nvim_lsp').default_capabilities()
+            })
 
-            require("mason-lspconfig").setup {
+            require('mason-lspconfig').setup({
                 ensure_installed = {
-                    "pyright",
-                },
+                "pyright",
+            },
                 handlers = {
                     -- this first function is the "default handler"
                     -- it applies to every language server without a "custom handler"
                     function(server_name)
-                        require("lspconfig")[server_name].setup {}
+                        require('lspconfig')[server_name].setup({})
                     end,
-                },
-            }
+                }
+            })
 
             local lspconfig = require "lspconfig"
 
@@ -147,6 +148,6 @@ return {
                     filetypes = { "python" },
                 }
             end
-        end,
-    },
+        end
+    }
 }
