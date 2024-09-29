b061f6cdd6254483a6073c370072d09773933ba0
Gibirizon <swojteksz@gmail.com>
2024-09-29 21:17:02 +0200

    added jupynium to nvim config

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -28,12 +28,6 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- Navigate vim panes better
--- vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
--- vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
--- vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
--- vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
-
 -- run python fast
 vim.keymap.set("n", "<leader>r", function()
     local file_dir = vim.fn.expand('%:p:h')
---
+++ nvim/.config/nvim/lua/plugins/jupynium.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "kiyoon/jupynium.nvim",
+        build = "pip3 install --user .",
+        -- build = "conda run --no-capture-output -n jupynium pip install .",
+        -- enabled = vim.fn.isdirectory(vim.fn.expand "~/python"),
+    },
+    "rcarriga/nvim-notify",   -- optional
+    "stevearc/dressing.nvim", -- optional, UI for :JupyniumKernelSelect
+}
